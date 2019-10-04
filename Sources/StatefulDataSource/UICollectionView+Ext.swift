c34199068e6c2d6bf1a91fbb33ba73689f93e314
Eugene Peschard <e.peschard@scrm.eu>
2019-10-04 17:37:04 +0200

    add extension helper for UICollectionView

---
+++ Sources/StatefulDataSource/UICollectionView+Ext.swift
@@ -0,0 +1,46 @@
+//
+//  UICollectionView+Extension.swift
+//  StatefulDataSource
+//
+//  Created by Eugène Peschard on 04/10/2019.
+//  Copyright © 2019 pesch.app All rights reserved.
+//
+
+#if !os(macOS)
+import UIKit
+
+extension UICollectionView {
+
+    func register<T: UICollectionViewCell>(reusable: T.Type) where T: ViewDataReusable {
+        switch T.reuseType {
+        case .classReference(let className):
+            self.register(className, forCellWithReuseIdentifier: T.reuseIdentifier)
+        case .nib(let nib):
+            self.register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
+        }
+    }
+
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T where T: ViewDataReusable {
+        guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Did you register this cell?")
+        }
+        return cell
+    }
+
+    func showEmptyView(with message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont.systemFont(ofSize: 19.0, weight: .light)
+        messageLabel.sizeToFit()
+
+        self.backgroundView = messageLabel;
+    }
+
+    func removeEmptyView() {
+        self.backgroundView = nil
+    }
+}
+#endif
